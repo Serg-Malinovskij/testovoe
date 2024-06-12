01fc7cb93206b229037cddd0b95e5f8c0f7df96b
Serg <malinovskij.serg@mail.ru>
2024-06-12 23:23:32 +0300

    complete js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 "use strict"
 
-// inputMask - находим все инпуты с типом tel на странице:
-let inputs = document.querySelectorAll('input[type="tel"]');
-// создадим экземпляр объекта Inputmask: передаем строку с маской где 9 -пустое значение - означает любое число
-let im = new Inputmask('+7 (999) 999-99-99');
-// к созданному экземпляру применим метод mask(), в который передадим наши инпуты inputs
-im.mask(inputs);
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
   
@@ -15,30 +7,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (document.querySelector("#popup")) {
 
-    const salePopupTrigger = document.getElementById('salebutton');
-    const salePopup = document.getElementById('salepopup');
-    const salePopupClose = document.getElementById('popup-close');
+    const popupTrigger = document.getElementById('popuptrigger');
+    const popup = document.getElementById('popup');
+    const popupClose = document.getElementById('popupclose');
 
-    salePopupTrigger.addEventListener('click', function () {
-      salePopup.style.display = "block";
+    popupTrigger.addEventListener('click', function () {
+      popup.style.display = "block";
+      document.body.classList.add('_lock');
     })
 
-    salePopupClose.addEventListener('click', function () {
-      salePopup.style.display = "none";
+    popupClose.addEventListener('click', function () {
+      popup.style.display = "none";
+      document.body.classList.remove('_lock');
     })
 
-    if (salePopup) {
+    if (popup) {
       document.addEventListener('click', (e) => {
-        if (!e.target.closest('#salepopup') && (!e.target.closest('#salebutton'))) {
-          salePopup.style.display = "none";
+        if (!e.target.closest('#popup') && (!e.target.closest('#popuptrigger'))) {
+          popup.style.display = "none";
+          document.body.classList.remove('_lock');
         }
       })
     }
-    function showSalePopup() {
-      salePopup.style.display = "block";
-    }
-
-    setTimeout(showSalePopup, 10000);
   }
 });
 
